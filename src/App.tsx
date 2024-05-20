5034c3fb62aec04df1131dca38d35ebd2a941721
micapl <michal.madziewicz@gmail.com>
2024-05-20 21:27:23 +0200

    Add button disable onclick (subsite selection pre), Dark Mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,132 @@
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  ButtonGroup,
+  Button,
+  Checkbox,
+} from "@nextui-org/react";
+import React, { useState } from "react";
+import useDarkMode from "use-dark-mode";
 
-function Top() {
-  return (
-    <Navbar>
-    <NavbarBrand>
-      <p className="font-bold text-inherit">Kasa Simulejtor</p>
-    </NavbarBrand>
-    <NavbarContent className="lg:flex gap-4" justify="center">
-      <NavbarItem>
-        <Button color="secondary" size="lg" href="#" variant="shadow">
-          Wpłaty
-        </Button>
-      </NavbarItem>
-      <NavbarItem>
-      <Button color="secondary" size="lg" href="#" variant="shadow">
-          Wypłaty
-        </Button>
-      </NavbarItem>
-      <NavbarItem>
-      <Button color="secondary" size="lg" href="#" variant="shadow">
-          Inwentaryzacja
-        </Button>
-      </NavbarItem>
-      <NavbarItem className="lg:flex">
-      <Button color="secondary" size="lg" href="#" variant="shadow">
-          Raporty
-        </Button>
-      </NavbarItem>
-    </NavbarContent>
-  </Navbar>
-);
+export const App = () =>{
+  const darkMode = useDarkMode(false);
+  return(
+    <main className={`${darkMode.value ? 'dark' : ''}`}>
+      <Top />
+    </main>
+  )
+ 
 }
 
-export default Top;
+
+
+
+export const Top = () => {
+  const darkMode = useDarkMode(false);
+  const [Disabled1, setDisabled1] = useState(false);
+  const [Disabled2, setDisabled2] = useState(false);
+  const [Disabled3, setDisabled3] = useState(false);
+  const [Disabled4, setDisabled4] = useState(false);
+  function DisableMe(event: React.MouseEvent<HTMLButtonElement>) {
+    switch (event.currentTarget.id) {
+      case "Button1":
+        setDisabled1(true);
+        setDisabled2(false);
+        setDisabled3(false);
+        setDisabled4(false);
+        break;
+      case "Button2":
+        setDisabled1(false);
+        setDisabled2(true);
+        setDisabled3(false);
+        setDisabled4(false);
+        break;
+      case "Button3":
+        setDisabled1(false);
+        setDisabled2(false);
+        setDisabled3(true);
+        setDisabled4(false);
+        break;
+      case "Button4":
+        setDisabled1(false);
+        setDisabled2(false);
+        setDisabled3(false);
+        setDisabled4(true);
+        break;
+      default:
+        break;
+    }
+  }
+
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <p style={{ fontSize: 25 }} className="font-bold text-foreground">Kasa Simulejtor</p>
+      </NavbarBrand>
+      <NavbarContent className="lg:flex gap-4" justify="center">
+        <NavbarItem>
+          <ButtonGroup>
+            <Button
+              color="secondary"
+              id="Button1"
+              size="lg"
+              href="#"
+              variant="shadow"
+              onClick={DisableMe}
+              isDisabled={Disabled1}
+            >
+              Wpłaty
+            </Button>
+            <Button
+              color="secondary"
+              id="Button2"
+              size="lg"
+              href="#"
+              variant="shadow"
+              onClick={DisableMe}
+              isDisabled={Disabled2}
+            >
+              Wypłaty
+            </Button>
+          </ButtonGroup>
+        </NavbarItem>
+        <NavbarItem>
+          <Button
+            color="secondary"
+            id="Button3"
+            size="lg"
+            href="#"
+            variant="shadow"
+            onClick={DisableMe}
+            isDisabled={Disabled3}
+          >
+            Inwentaryzacja
+          </Button>
+        </NavbarItem>
+        <NavbarItem className="lg:flex">
+          <Button
+            color="secondary"
+            id="Button4"
+            size="lg"
+            href="#"
+            variant="shadow"
+            onClick={DisableMe}
+            isDisabled={Disabled4}
+          >
+            Raporty
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Checkbox defaultSelected onValueChange={darkMode.toggle}></Checkbox>
+        </NavbarItem>
+      </NavbarContent>
+      
+    </Navbar>
+  );
+};
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Top from './App';
+import {App} from './App';
 import reportWebVitals from './reportWebVitals';
 import {NextUIProvider} from "@nextui-org/react";
 
@@ -9,9 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <NextUIProvider>
-      <main className="dark">
-        <Top />
-      </main>
+      <App/>
     </NextUIProvider>
   </React.StrictMode>
 );
